66aee3be1e0ad1cc3f66953d235dcc7027367f5d
Jacob Waller <jacob.waller@logica.com>
2012-03-15 17:33:54 +0100

    Updated API with basic handling of North Partner Organisation

--- index.js
+++ index.js
@@ -16,10 +16,33 @@ var Tree = new mongoose.Schema({
 mongoose.model('Tree', Tree);
 
 
+var NorthPartnerOrganisation = new mongoose.Schema({
+    // objectId: mongoose.Schema.ObjectId,
+    // name: {
+    //     type: String,
+    //     index: {
+    //         unique: false
+    //     }
+    // },
+    name: String,
+    email: String,
+    phone: String,
+    date: Date
+});
+mongoose.model('NorthPartnerOrganisation', NorthPartnerOrganisation);
+
 // Server
 var app = express.createServer();
+app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.session({ secret: 'secret' }));
+
+// CORS FTW
+app.all('/api/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    next();
+});
 
 
 // Routes
@@ -27,26 +50,40 @@ app.get('/', function(req, res){
     res.send('Hello world!');
 });
 
-app.get('/api/tree/:title', function(req, res){
-
-    res.send(mongoose.model('Tree').find({ title: req.params.title }));
+app.get('/api/northpartnerorganisation', function(req, res){
+    res.send(mongoose.model('NorthPartnerOrganisation').find());
 });
 
-app.post('/api/tree/:title', function(req, res){
+app.get('/api/northpartnerorganisation/:name', function(req, res){
+    res.send(mongoose.model('NorthPartnerOrganisation').find({ name: req.params.name }));
+});
 
-    var tree = new (mongoose.model('Tree'))({
-        title: req.params.title,
-        date: new Date()
-    });
 
-    tree.save(function(err) {
-        if (err) {
-            res.send(err);
-        } else {
-            req.flash('notice', 'Created successfully');
-            res.redirect('/api/tree/' + tree.title);
-        }
-    });
+function onSave(err){
+    if (err) {
+        console.log('err', err);
+        this.send({ success: false });
+    } else {
+        this.send({ success: true });
+    }
+}
+app.post('/api/northpartnerorganisation', function(req, res){
+
+    // console.log('before manip', req.body);
+
+    var id = req.body._id;
+    delete req.body._id;
+    delete req.body.id;
+
+    //FIXME this should be fixed on the client so a request with an existing id uses PUT
+    if(id){
+        console.log('updating', id, req.body);
+        mongoose.model('NorthPartnerOrganisation').update({ _id: id }, req.body, onSave.bind(res));
+    } else {
+        console.log('saving', req.body);
+        var northPartnerOrganisation = new (mongoose.model('NorthPartnerOrganisation'))(req.body);
+        northPartnerOrganisation.save(onSave.bind(res));
+    }
 
 });
 
